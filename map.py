efac72047ce7276de17d0607888c95f5b7828559
Guru-Naresh <46043215+Guru-Naresh@users.noreply.github.com>
2019-06-19 18:14:04 +0530

    Update map.py

--- map.py
+++ map.py
@@ -4,6 +4,6 @@ import json
 api_key = 'AIzaSyBgLnebInihBBk_qifMc7tcZcQ57T4RcyY'
 url = 'https://maps.googleapis.com/maps/api/geocode/json?'
 
-res_ob = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address=New+york&key=AIzaSyBgLnebInihBBk_qifMc7tcZcQ57T4RcyY')
+res_ob = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address=New+york&key="API_ID"')
 x = res_ob.json()
 print(x)
